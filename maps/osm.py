d711b833f85335d27a331ddd2ba82e8fa8fe6d31
Kharude, Sachin <sachin.kharude@here.com>
2021-01-29 15:19:44 +0530

    initial changes for overpass
    Signed-off-by: Kharude, Sachin <sachin.kharude@here.com>

--- maps/osm.py
+++ maps/osm.py
@@ -2,7 +2,10 @@
 import json
 
 import click
+import overpy
+import pandas as pd
 from geopy.geocoders import Nominatim
+from tabulate import tabulate
 
 from maps import __version__
 from maps.utils import yield_subcommands
@@ -45,3 +48,37 @@ def geocoding(query, forward, raw):
             click.secho(json.dumps(location.raw, indent=2), fg="green")
         else:
             click.secho(location.address, fg="green")
+
+
+@osm.command(short_help="OSM's Overpass API")
+@click.argument("query", required=True)
+def overpass(query):
+    """OSM's Overpass API service."""
+    api = overpy.Overpass()
+    result = api.query(query)
+    node_tags = []
+    for node in result.nodes:
+        node.tags["latitude"] = node.lat
+        node.tags["longitude"] = node.lon
+        node.tags["id"] = node.id
+        node_tags.append(node.tags)
+    nodes_df = pd.DataFrame(node_tags)
+    if not nodes_df.empty:
+        click.secho("Nodes:", fg="green")
+        click.secho((tabulate(nodes_df, headers="keys", tablefmt="psql")), fg="green")
+    ways = []
+    for way in result.ways:
+        way.tags["id"] = way.id
+        ways.append(way.tags)
+    ways_df = pd.DataFrame(ways)
+    if not ways_df.empty:
+        click.secho("Ways:", fg="green")
+        click.secho((tabulate(ways_df, headers="keys", tablefmt="psql")), fg="green")
+    relations = []
+    for relation in result.relations:
+        relation.tags["id"] = relation.id
+        relations.append(relation.tags)
+    relations_df = pd.DataFrame(relations)
+    if not relations_df.empty:
+        click.secho("Relations:", fg="green")
+        click.secho((tabulate(relations_df, headers="keys", tablefmt="psql")), fg="green")
