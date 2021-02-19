81ce63fcdb52c422d5b9da96d679486fcf4cc2e8
Kharude, Sachin <sachin.kharude@here.com>
2021-02-20 00:25:36 +0530

    Added discover command for here provider
    Signed-off-by: Kharude, Sachin <sachin.kharude@here.com>

--- maps/here.py
+++ maps/here.py
@@ -5,6 +5,7 @@ import os
 import click
 from geojsonio import display as geo_display
 from geopy.geocoders import Here
+from here_location_services import LS
 
 from maps.exceptions import ApiKeyNotFoundError
 from maps.utils import get_feature_from_lat_lon, yield_subcommands
@@ -73,3 +74,83 @@ def geocoding(ctx, query, apikey, forward, raw, display):
             click.secho(json.dumps(location.raw, indent=2), fg="green")
         else:
             click.secho(location.address, fg="green")
+
+
+@here.command(short_help="Search places using free-form text query.")
+@click.argument("query", required=True)
+@click.option(
+    "--coordinates",
+    help="A {longitude,latitude} coordinate pair around which search query will be applied.",
+    type=str,
+)
+@click.option(
+    "--radius", help="radius in meters along with coordinates for searching places.", type=str
+)
+@click.option(
+    "--country_codes", help="comma separated ISO 3166-1 alpha-3 country codes.", type=str
+)
+@click.option(
+    "--bounding_box",
+    help="A bounding box, provided as {west longitude},{south latitude},{east longitude},"
+    "{north latitude}",
+    type=str,
+)
+@click.option("--limit", help="Maximum numbers of results to be returned.", type=str)
+@click.option(
+    "--lang",
+    help="language to be used for result rendering from a list of BCP47 compliant Language Codes.",
+    type=str,
+)
+@click.option("--apikey", help="Your HERE API key", type=str)
+@click.option("--raw", is_flag=True)
+@click.option("--display", help="Display result in browser", is_flag=True)
+@click.pass_context
+def discover(
+    ctx, query, coordinates, radius, country_codes, bounding_box, limit, lang, apikey, raw, display
+):
+    """
+    Search places using free-form text query.
+    There are multiple combination of inputs for restricting your search
+
+    - ``center`` and ``country_code``
+    - ``center`` and ``radius``
+    - ``bounding_box``
+
+    \f
+
+    :param ctx:
+    :param query:
+    :param coordinates:
+    :param radius:
+    :param country_codes:
+    :param bounding_box:
+    :param limit:
+    :param lang:
+    :param apikey:
+    :param raw:
+    :param display:
+    :return:
+    """
+    apikey = apikey or os.environ.get("HERE_APIKEY")
+    if apikey is None:
+        raise ApiKeyNotFoundError(
+            "Please pass HERE API KEY as --apikey or set it as environment "
+            "variable in HERE_APIKEY "
+        )
+    ctx.obj["apikey"] = apikey
+    ls = LS(api_key=apikey)
+    result = ls.discover(
+        query=query,
+        center=coordinates.split(",")[::-1] if coordinates else coordinates,
+        radius=radius,
+        country_codes=country_codes.split(",") if country_codes else country_codes,
+        bounding_box=bounding_box.split(",") if bounding_box else bounding_box,
+        limit=limit,
+        lang=lang,
+    )
+    if raw:
+        click.secho(json.dumps(result.response, indent=2), fg="green")
+    elif display:
+        geo_display(json.dumps(result.to_geojson(), indent=2))
+    else:
+        click.secho(json.dumps(result.items, indent=2), fg="green")
--- tests/test_here.py
+++ tests/test_here.py
@@ -10,7 +10,7 @@ def test_show():
     """Test here show command."""
     runner = CliRunner()
     result = runner.invoke(maps, ["here", "show"], catch_exceptions=False)
-    assert result.output == "geocoding\n"
+    assert result.output.split() == ["geocoding", "discover"]
 
 
 def test_geocoding_fwd():
@@ -34,3 +34,41 @@ def test_geocoding_reverse():
     )
     assert result.exit_code == 0
     assert "I B Patel Road" in result.output
+
+
+def test_discover():
+    runner = CliRunner()
+    result = runner.invoke(
+        maps,
+        ["here", "discover", "starbucks", "--coordinates=72.8526,19.1663", "--country_codes=IND"],
+    )
+    assert result.exit_code == 0
+    assert "Starbucks, New Link Road, Andheri West, Mumbai 400053, India" in result.output
+
+    result2 = runner.invoke(
+        maps,
+        [
+            "here",
+            "discover",
+            "coffee",
+            "--coordinates=13.38469,52.53086",
+            "--radius=1000",
+            "--raw",
+        ],
+    )
+    assert result2.exit_code == 0
+    assert '"title": "Eins"' in result2.output
+
+    result3 = runner.invoke(
+        maps,
+        [
+            "here",
+            "discover",
+            "starbucks",
+            "--bounding_box=13.08836,52.33812,13.761,52.6755",
+            "--limit=1",
+            "--lang=en-US",
+        ],
+    )
+    assert result3.exit_code == 0
+    assert "Starbucks, Am Ostbahnhof, 10243 Berlin, Germany" in result3.output
